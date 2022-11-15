f96d7dd4b2a8e869479b93368cc7581aa081586e
DmitryBalkonsky <dima.balynin2@gmail.com>
2022-11-15 20:25:42 +0300

    Nadeys nirm

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,11 +12,11 @@ public class Main {
         userService.createUsersTable();
         userService.saveUser("dima","balynin", (byte) 26);
         System.out.println("пользователь с именем – dima добавлен в базу данных");
-        userService.saveUser("dima1","balynin1", (byte) 27);
+        userService.saveUser("dima1","balynin1", (byte) 28);
         System.out.println("пользователь с именем – dima1 добавлен в базу данных");
-        userService.saveUser("dima2","balynin2", (byte) 28);
+        userService.saveUser("dima2","balynin2", (byte) 29);
         System.out.println("пользователь с именем – dima2 добавлен в базу данных");
-        userService.saveUser("dima3","balynin3", (byte) 29);
+        userService.saveUser("dima3","balynin3", (byte) 30);
         System.out.println("пользователь с именем – dima3 добавлен в базу данных");
         List<User> userList = userService.getAllUsers();
         userList.forEach(System.out::println);
